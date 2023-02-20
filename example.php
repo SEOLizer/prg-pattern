6f69c6b10ce6ba30feea28b84a156d3cfe8a468b
roene@3b-kommunikation.de <roene@3b-kommunikation.de>
2023-02-20 19:20:09 +0100

    Add PHP example

--- example.php
+++ example.php
@@ -1,21 +1,24 @@
 <html>
 <head>
-    <title>PRG-Pattern - Static Demo</title>
+    <title>PRG-Pattern - PHP Demo</title>
     <script src="js/jquery.min.js"></script>
     <link rel="stylesheet" href="css/prg.css" type="text/css"/>
 </head>
 <body>
+<ul>
+    <li>
+        <a href="example.html">First Link to static demo</a>
+    </li>
+    <li>
+        <?php
 
-<div>
-    <ul>
-        <li>
-            <a href="example.html">First Link to static demo</a>
-        </li>
-        <li>
-            <span class="prglink " data-adr="aHR0cDovL2xvY2FsaG9zdC9leGFtcGxlLnBocA==" data-hash="09ae9a5fd400673b6a303232aa4497e3" tabindex="0" onkeypress="if(event.keyCode==13)this.click()">Secound Link as PRG-Pattern</span>    </li>
-        </li>
-    </ul>
-</div>
+        require_once('libs/prglib.php');
+
+        echo(createPRGTag('http://localhost/example.php','saltvalue','Secound Link as PRG-Pattern',''));
+
+        ?>
+    </li>
+</ul>
 <!-- PRG-Form Start -->
 <span class="prgform" style="display:none">
     <form action="/prg.php" method="post" id="prgform">
